6c8f4df32d8fb1df9136f49100328420aa385bf8
kurn14 <kurn14.why@gmail.com>
2026-08-10 10:33:34 +0700

    Tambahkan translasi pada label, form, resource, table, dsb

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
-            $switch->locales(['id', 'en']);
+            $switch->locales(['id', 'en', 'ar', 'es']);
         });
     }
 }
